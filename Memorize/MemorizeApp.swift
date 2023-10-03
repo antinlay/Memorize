5ad790aaba8ef3cee4d972fad5b565f3182b691f
janiecee <antinlay@gmail.com>
2023-10-03 19:23:59 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Janiece Eleonor on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
