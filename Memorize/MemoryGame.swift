a740052924e01400efaed488781101c15f7be42a
janiecee <antinlay@gmail.com>
2023-10-25 14:38:01 +0300

    Add deal cards animation

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -40,7 +40,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
                     if cards[choosenIndex].content == cards[potentialMatchIndex].content {
                         cards[choosenIndex].isMatched = true
                         cards[potentialMatchIndex].isMatched = true
-                        score += 2
+                        score += 2 + cards[choosenIndex].bonus + cards[potentialMatchIndex].bonus
                     } else {
                         if cards[choosenIndex].hasBeenSeen {
                             score -= 1
@@ -67,66 +67,58 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
         var isFaceUp = false {
             didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                }
                 if oldValue && !isFaceUp {
                     hasBeenSeen = true
                 }
             }
         }
         var hasBeenSeen = false
-        var isMatched = false
+        var isMatched = false {
+            didSet {
+                if isMatched {
+                    stopUsingBonusTime()
+                }
+            }
+        }
         let content: CardContent
         
         // MARK: - Bonus Time
         
-        // this could give matching bonus points
-        // if the user matches the card
-        // before a certain amount of time passes during which the card is face up
-        
-        // can be zero which means "no bonus available" for this card
-        var bonusTimeLimit: TimeInterval = 10
-        
-        // how long this card has ever been face up
-        private var faceUpTime: TimeInterval {
+        var lastFaceUpDate: Date?
+        var faceUpTime: TimeInterval {
             if let lastFaceUpDate = self.lastFaceUpDate {
                 return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
             } else {
                 return pastFaceUpTime
             }
         }
-        // the last time this card was turned face up (and is still face up)
-        var lastFaceUpDate: Date?
-        // the accumulated  time yhis card has been face up in the past
-        // (i.e. not including the current time it`s been face up if it is currently so)
         var pastFaceUpTime: TimeInterval = 0
-        
-        // how much time left before the bonus opportunity runs out
-        var bonusTimeRemaining: TimeInterval {
-            max(0, bonusTimeLimit - faceUpTime)
-        }
-        // percentage of the bonus time remaining
-        var bonusRemaining: Double {
-            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
-        }
-        // whether the card was matched during the bonus time period
-        var hasEarnedBonus: Bool {
-            isMatched && bonusTimeRemaining > 0
-        }
-        // whether we are currently face up, unmatched and have not yet used up the bonus window
-        var isConsumingBonusTime: Bool {
-            isFaceUp && !isMatched && bonusTimeRemaining > 0
-        }
+        var bonusTimeLimit: TimeInterval = 6
         
         // called when the card transitions to face up state
         private mutating func startUsingBonusTime() {
-            if isConsumingBonusTime, lastFaceUpDate == nil {
+            if isFaceUp && !isMatched && bonusPercentRemaining > 0, lastFaceUpDate == nil {
                 lastFaceUpDate = Date()
             }
         }
         // called when the card goes back face down (or gets matched)
         private mutating func stopUsingBonusTime() {
             pastFaceUpTime = faceUpTime
-            self.lastFaceUpDate = nil
+            lastFaceUpDate = nil
         }
+        
+        var bonus: Int {
+            Int(bonusTimeLimit * bonusPercentRemaining)
+        }
+        
+        var bonusPercentRemaining: Double {
+            bonusTimeLimit > 0 ? max(0, bonusTimeLimit - faceUpTime) / bonusTimeLimit : 0
+        }
+        
+        
         
         var id: String
         var debugDescription: String {
